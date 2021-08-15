d1b4b36cefaeb7d2efbccfcfe45c15bd799c7bd0
Allan Amaral <allanmaralr@gmail.com>
2021-08-15 15:42:50 -0300

    demo now using ui package component

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -2,12 +2,15 @@
   <div>
     <v-app>
       <v-main>
+        <v-container>
+          <sample-component></sample-component>
+        </v-container>
         <v-container>Hello world</v-container>
         <v-container>
-          <bc-btn color="primary">test</bc-btn>
-          <bc-btn color="dark">test</bc-btn>
-          <bc-btn color="link-blue">test</bc-btn>
-          <bc-btn color="link-gray">test</bc-btn>
+          <hc-btn color="primary">test</hc-btn>
+          <hc-btn color="dark">test</hc-btn>
+          <hc-btn color="link-blue">test</hc-btn>
+          <hc-btn color="link-gray">test</hc-btn>
         </v-container>
       </v-main>
     </v-app>
@@ -16,12 +19,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue-demi'
-import BcBtn from './components/hc-btn/HcBtn.vue'
+import {SampleComponent} from '@heart/ui'
+import HcBtn from './components/hc-btn/HcBtn.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    BcBtn
+    HcBtn,
+    SampleComponent
   }
 })
 </script>
