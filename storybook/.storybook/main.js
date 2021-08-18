810d49a70bcea26cfe4e78d5b49481e821328348
Allan Amaral <allanmaralr@gmail.com>
2021-08-17 23:28:56 -0300

    move storybook folder

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-links",
